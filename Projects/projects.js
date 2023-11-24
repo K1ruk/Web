33b4f90ccf3c0042021dbcde8cb50535b2bc4233
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-11-24 23:00:27 +0100

    Add files via upload

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,12 +1,12 @@
+// Defining variables for later use
 var myElement = document.getElementById('nav-menu-mobile');
 var addButton = document.getElementById('menu');
-
 var Addoverlay = document.getElementById("overlay");
-var body = document.getElementById("body")
+var body = document.getElementById("body");
 
 // Add an event listener to the menu button
 addButton.addEventListener('click', function() {
-
+    
   // Add both nav-menu-mobile and nav-menu-mobile-active to the existing classes when the button is clicked
   myElement.classList.add('nav-menu-mobile-active');
 });
